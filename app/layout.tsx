a397834708d437519f023b7ecffe4f3687c53993
mood <mooddddd@naver.com>
2023-11-13 17:22:24 +0900

    feat: toaster 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Nunito } from 'next/font/google'; // 폰트 추가
 import type { Metadata } from 'next';
 import { ClientOnly, Navbar, RegisterModal } from './components';
+import { ToasterProvider } from './providers';
 
 export const metadata: Metadata = {
   title: 'Airbnb', // 브라우저 랜더시 타이틀
@@ -21,6 +22,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          {/* Toaster를 바로 가져오면 해당 컴포넌트는 useEffect 등을 사용해야 하기 때문에 
+          레이아웃 컴포넌트에서는 사용이 불가능.
+          그래서 use client인 provider 컴포넌트를 하나 만들어주고 가져와야 사용 가능? */}
           <RegisterModal />
           <Navbar />
         </ClientOnly>
